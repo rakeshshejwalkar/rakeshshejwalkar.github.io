828be3b8a01f287fb09c69a260fd3f348d432ccc
Rakesh Shejwalkar <rakesh.kgp2003@gmail.com>
2021-05-02 23:50:51 +0530

    Updates

---
+++ precache-manifest.08427f168a5fdedca3828d63df17cf27.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afc80e9bb69a1315d619c79b81b8f8a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e6ca727422769170b28",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e6ca727422769170b28",
+    "url": "/static/js/main.3ab9b129.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "69d3ef8e355824bd1dcacfd4ccde5b4d",
+    "url": "/static/media/rakeshshejwalkar.69d3ef8e.jpg"
+  },
+  {
+    "revision": "dd10ca4b2e2e931b3d51caaff044ece0",
+    "url": "/static/media/resume.dd10ca4b.pdf"
+  }
+]);
